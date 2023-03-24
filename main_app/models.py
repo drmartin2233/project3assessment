5a27a96e75a5a97c07cf7af93a8d6f28c50af9a9
David Martin <d.r.martin.wrk@gmail.com>
2023-03-24 17:23:09 -0400

    stuck

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from . import views 
 
-# Create your models here.
+# Create your models here
+class Item(models.Model):
+    description = models.CharField(max_length=50)
--- project3assessment/urls.py
+++ project3assessment/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from . import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
+    path('items/', views.item)
 ]
